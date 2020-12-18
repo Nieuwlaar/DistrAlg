0975316ba21c893eac7bba668336d04574ac8da6
Nieuwlaar <nieuwlaar@gmail.com>
2020-12-18 16:08:52 +0100

    DA - Exercise 3

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # Press the green button in the gutter to run the script.
-
+import socket
 
 
 def broadcast(round, v):
@@ -11,28 +11,37 @@ class Node(object):
         self.i = i
         self.address = address
         print(i)
-        address = 12345+i
+        address = 12345 + i
         print(address)
 
 
-
-def initializeNodes(total_nodes, failed_nodes):
+def initialize_nodes(total_nodes, failed_nodes):
     i = 1
     nodes = []
     starting_port = 12345
     while i < total_nodes:
-        nodes.append(Node(i, starting_port+i))
-
+        nodes.append(Node(i, starting_port + i))
         i = i + 1
+    return nodes
 
-    # print(nodes[])
+
+def connect_nodes(nodes):
+    s = socket.socket()
+    n = 5
+    connections = []
+    for obj in nodes:
+        port = obj.address
+        s.bind(('', port))
+        s.listen(n)
+        n = n + 1
 
 
 if __name__ == '__main__':
     # parameters:
     total_nodes = 7  # number of processors
     failed_nodes = 1  # number of failures
-    initializeNodes(total_nodes, failed_nodes)
+    nodes = initialize_nodes(total_nodes, failed_nodes)
+    connect_nodes(nodes)
 
     round = 1
     decided = False
